730fcc47335209357fbbe4b57bf4e22420d78e71
Lovepreet <gillpreetsingh35@gmail.com>
2026-01-27 18:03:25 -0500

    .env added to gitignore

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://159.203.43.122:5000/api",
 })
 
 api.interceptors.request.use((config) => {
