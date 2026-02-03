2cff5bdfd184dd546adc143a1e2aaee61b83531b
Lovepreet <gillpreetsingh35@gmail.com>
2026-02-02 22:35:01 -0500

    removed dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+document.documentElement.classList.remove("dark") // ensure no flash of dark mode on initial load
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
