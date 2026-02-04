d4838455c7c1b5866b65ed7c5e6cf94e4b07ea10
Lovepreet <gillpreetsingh35@gmail.com>
2026-02-04 16:39:28 -0500

    add icon to the tab and upadte api url to env

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,8 @@
 import axios from "axios"
+const API_URL = import.meta.env.VITE_API_URL // ensure env vars are loaded
 
 const api = axios.create({
-  baseURL: "http://159.203.43.122:5000/api",
+  baseURL: API_URL,
 })
 
 api.interceptors.request.use((config) => {
